49a7b3b26208eb080864fcbdf919d1761b377146
Lucas Rocha <lucasrochadoo123@gmail.com>
2024-09-25 11:22:13 -0300

    correção de alguns erros

--- src/paginas/listaProdutos/index.tsx
+++ src/paginas/listaProdutos/index.tsx
@@ -22,7 +22,7 @@ interface Produto{
 const ITENS_P_PAGINA = 20
 
 function Inicial(){
-    const {token,email} = useAuth()
+    const {token} = useAuth()
 
     const [categorias, setCategorias] = useState<string[]>([])
 
